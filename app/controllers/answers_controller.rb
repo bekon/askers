c1a981f13c494007adb32139682d7d4fa0f7bcaf
Evgeniy Babin <evgeniy.bekon@gmail.com>
2015-08-25 00:11:50 +0600

    Added answers to the questions page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,34 +1,15 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
-
-  # GET /answers
-  # GET /answers.json
-  def index
-    @answers = Answer.all
-  end
-
-  # GET /answers/1
-  # GET /answers/1.json
-  def show
-  end
-
-  # GET /answers/new
-  def new
-    @answer = Answer.new
-  end
-
-  # GET /answers/1/edit
-  def edit
-  end
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
 
   # POST /answers
   # POST /answers.json
   def create
     @answer = Answer.new(answer_params)
+    @answer.user_id = current_user.id
 
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+        format.html { redirect_to controller: 'questions', action: 'show', id: @answer.question_id }
         format.json { render :show, status: :created, location: @answer }
       else
         format.html { render :new }
@@ -62,11 +43,6 @@ class AnswersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_answer
-      @answer = Answer.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def answer_params
       params.require(:answer).permit(:user_id, :question_id, :content)
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :question_votes
+  has_many :answers
 end
